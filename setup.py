7219916725ab3ac439ee618230c772b55f9ab578
wakiliAI <45507531+legalTech-Kenya@users.noreply.github.com>
2019-01-20 11:08:47 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys, os
 from idna import idnadata
 from multiprocessing import Queue
 import os
+import csv
 
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
@@ -25,20 +26,21 @@ if sys.platform=="win64":
     base="Win64GUI"
 
 exe = Executable(script='LegiBot.py', base = base, icon='scrapercon.ico')
-
+includefiles = ['blacks_Law_Dict.csv']
 additional_mods = ['numpy.core._methods', 'numpy.lib.format']
-additional_packages = ['asyncio', 'asyncio.compat', 'appdirs', 'pkg_resources._vendor', 'tkinter']
+additional_packages = ['asyncio', 'asyncio.compat', 'appdirs', 'csv', 'pkg_resources._vendor', 'tkinter']
 
 setup(
-    name = "LegiBot.py",
-    version = "1.0",
-    description = 'Legislative Bot',
+    name = "Desktop Professional Support Lawyer",
+    version = "1.0.1",
+    description = 'Desktop Professional Support Lawyer.',
     author = 'Wakili.AI',
     author_email = 'hello@nomeon.technology',
     options = {
         'build_exe': {
             'includes': additional_mods,
             'packages': additional_packages,
+            'include_files':includefiles,
             'include_files':[
                 os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
                 os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
